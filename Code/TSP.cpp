9ca8a2e49ef2ea2caf3eb1509012608a3b1299f4
Chao PAN <54287779+chaopan1995@users.noreply.github.com>
2020-03-30 22:57:12 +0200

    Update TSP.cpp

--- Code/TSP.cpp
+++ Code/TSP.cpp
@@ -12,33 +12,30 @@ int in[1000001] = {};
 int idx[1000001] = {};
 struct Village
 {
-    int id = -1;
-    int dp = 1;
-    Village *next = nullptr;
+    int id;
+    Village *next_village;
+    Village (int i, Village *next=nullptr):id(i), next_village(next){}
 };
 
 struct Villages
 {
-    Village *cur = nullptr;
+    int dp;
+    Village *next;
+    Villages():dp(1), next(nullptr){}
+    void connect(int);
 }v[1000001];
 
-void add(int i, int j)
+void Villages::connect(int j)
 {
     in[j]++;
-    if (v[i].cur==nullptr)
+    if (next==nullptr)
     {
-        v[i].cur = new Village{i};
+        next = new Village(j);
     }
-    if (v[j].cur==nullptr)
+    else
     {
-        v[j].cur = new Village{j};
+        next = new Village(j, next);
     }
-    Village *cur = v[i].cur;
-    while(cur->next!=nullptr)
-    {
-        cur = cur->next;
-    }
-    cur->next = new Village{j};
 }
 
 int max(int i, int j)
@@ -50,7 +47,7 @@ void topological_sort(int n)
 {
     int len = 0;
     int res = 0;
-    Village *cur;
+    Village *next;
     for (int i=0; i<n; i++)
     {
         if (!in[i])
@@ -60,143 +57,33 @@ void topological_sort(int n)
     }
     for (int i=0; i<len; i++)
     {
-        cur = v[idx[i]].cur->next;
-        while(cur!=nullptr)
+        next = v[idx[i]].next;
+        while(next!=nullptr)
         {
-            v[cur->id].cur->dp = max(v[idx[i]].cur->dp+1, v[cur->id].cur->dp);
-            in[cur->id]--;
-            if(!in[cur->id])
+            v[next->id].dp = max(v[idx[i]].dp+1, v[next->id].dp);
+            in[next->id]--;
+            if(!in[next->id])
             {
-                idx[len++] = cur->id;
+                idx[len++] = next->id;
             }
-            res = max(res, v[cur->id].cur->dp);
-            cur = cur->next;
+            res = max(res, v[next->id].dp);
+            next = next->next_village;
         }
     }
     std::cout << res << std::endl;
 }
 
-void print(int n)
-{
-    for (int i=0; i<1; i++)
-    {
-        std::cout << v[i].cur->id << " (in degree: " << in[i] << ")-> ";
-        Village *cur = v[i].cur->next;
-        while (cur!=nullptr)
-        {
-            std::cout << cur->id << " ";
-            cur = cur->next;
-        }
-        std::cout << std::endl;
-    }
-}
-/*
-class TSP
-{
-    Village **v;
-    int size;
-    int *in;
-    int *idx;
-public:
-    TSP(int _size)
-    {
-        size = _size;
-        v = new Village*[size];
-        for (int i=0; i<size; i++)
-        {
-            v[i] = new Village{i};
-        }
-        in = new int[size]{};
-        idx = new int[size]{};
-    }
-    ~TSP()
-    {
-        for (int i=0; i<size; i++)
-        {
-            remove_village(v[i]);
-        }
-        delete []v;
-        delete []in;
-        delete []idx;
-    }
-    void remove_village(Village *cur)
-    {
-        if (cur!=nullptr)
-        {
-            remove_village(cur->next);
-            delete cur;
-        }
-    }
-    void add(int i, int j)
-    {
-        in[j]++;
-        //v[i]->id = i;
-        Village *cur = v[i];
-        while(cur->next!=nullptr)
-        {
-            cur = cur->next;
-        }
-        cur->next = new Village{j};
-    }
-    void print()
-    {
-        for (int i=0; i<size; i++)
-        {
-            std::cout << v[i]->id << " (in degree: " << in[i] << ")-> ";
-            Village *cur = v[i]->next;
-            while (cur!=nullptr)
-            {
-                std::cout << cur->id << " ";
-                cur = cur->next;
-            }
-            std::cout << std::endl;
-        }
-    }
-    int max(int i, int j)
-    {
-        return i>j?i:j;
-    }
-    void topological_sort()
-    {
-        int len = 0;
-        Village *cur;
-        int res = 0;
-        for (int i=0; i<size; i++)
-        {
-            if (!in[i])
-            {
-                idx[len++] = i;
-            }
-        }
-        for (int i=0; i<len; i++)
-        {
-            cur = v[idx[i]]->next;
-            while (cur!=nullptr)
-            {
-                v[cur->id]->dp = max(v[idx[i]]->dp+1, cur->dp);
-                in[cur->id]--;
-                if (!in[cur->id])
-                {
-                    idx[len++] = cur->id;
-                }
-                res = max(res, v[cur->id]->dp);
-                cur = cur->next;
-            }
-        }
-        std::cout << res << std::endl;
-    }
-};
-*/
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     // Topological sorting
+
     int n, m, i, j;
     std::cin >> n >> m;
-    //TSP tsp(n);
     while(m--)
     {
         std::cin >> i >> j;
-        add(i-1, j-1);
+        v[i-1].connect(j-1);
     }
     topological_sort(n);
     return 0;
